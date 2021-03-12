2541953e2c92f1c3aa5049e2351959412d8a36c5
Cancan <cancan_huang@brown.edu>
2021-03-12 03:00:12 +0000

    github codespace test

--- project4-cnns-cancan233/code/preprocess.py
+++ project4-cnns-cancan233/code/preprocess.py
@@ -12,8 +12,9 @@ import tensorflow as tf
 
 import hyperparameters as hp
 
-class Datasets():
-    """ Class for containing the training and test sets as well as
+
+class Datasets:
+    """Class for containing the training and test sets as well as
     other useful data-related information. Contains the functions
     for preprocessing.
     """
@@ -37,12 +38,14 @@ class Datasets():
 
         # Setup data generators
         self.train_data = self.get_data(
-            os.path.join(self.data_path, "train/"), task == '3', True, True)
+            os.path.join(self.data_path, "train/"), task == "3", True, True
+        )
         self.test_data = self.get_data(
-            os.path.join(self.data_path, "test/"), task == '3', False, False)
+            os.path.join(self.data_path, "test/"), task == "3", False, False
+        )
 
     def calc_mean_and_std(self):
-        """ Calculate mean and standard deviation of a sample of the
+        """Calculate mean and standard deviation of a sample of the
         training dataset for standardization.
 
         Arguments: none
@@ -61,18 +64,17 @@ class Datasets():
         random.shuffle(file_list)
 
         # Take sample of file paths
-        file_list = file_list[:hp.preprocess_sample_size]
+        file_list = file_list[: hp.preprocess_sample_size]
 
         # Allocate space in memory for images
-        data_sample = np.zeros(
-            (hp.preprocess_sample_size, hp.img_size, hp.img_size, 3))
+        data_sample = np.zeros((hp.preprocess_sample_size, hp.img_size, hp.img_size, 3))
 
         # Import images
         for i, file_path in enumerate(file_list):
             img = Image.open(file_path)
             img = img.resize((hp.img_size, hp.img_size))
             img = np.array(img, dtype=np.float32)
-            img /= 255.
+            img /= 255.0
 
             # Grayscale -> RGB
             if len(img.shape) == 2:
@@ -90,14 +92,20 @@ class Datasets():
 
         # ==========================================================
 
-        print("Dataset mean: [{0:.4f}, {1:.4f}, {2:.4f}]".format(
-            self.mean[0], self.mean[1], self.mean[2]))
+        print(
+            "Dataset mean: [{0:.4f}, {1:.4f}, {2:.4f}]".format(
+                self.mean[0], self.mean[1], self.mean[2]
+            )
+        )
 
-        print("Dataset std: [{0:.4f}, {1:.4f}, {2:.4f}]".format(
-            self.std[0], self.std[1], self.std[2]))
+        print(
+            "Dataset std: [{0:.4f}, {1:.4f}, {2:.4f}]".format(
+                self.std[0], self.std[1], self.std[2]
+            )
+        )
 
     def standardize(self, img):
-        """ Function for applying standardization to an input image.
+        """Function for applying standardization to an input image.
 
         Arguments:
             img - numpy array of shape (image size, image size, 3)
@@ -111,8 +119,6 @@ class Datasets():
         #       the standardization.
         # =============================================================
 
-
-
         # =============================================================
 
         return img
@@ -120,20 +126,20 @@ class Datasets():
     def preprocess_fn(self, img):
         """ Preprocess function for ImageDataGenerator. """
 
-        if self.task == '3':
+        if self.task == "3":
             img = tf.keras.applications.vgg16.preprocess_input(img)
         else:
-            img = img / 255.
+            img = img / 255.0
             img = self.standardize(img)
         return img
 
     def custom_preprocess_fn(self, img):
         """ Custom preprocess function for ImageDataGenerator. """
 
-        if self.task == '3':
+        if self.task == "3":
             img = tf.keras.applications.vgg16.preprocess_input(img)
         else:
-            img = img / 255.
+            img = img / 255.0
             img = self.standardize(img)
 
         # EXTRA CREDIT:
@@ -148,14 +154,13 @@ class Datasets():
 
         if random.random() < 0.3:
             img = img + tf.random.uniform(
-                (hp.img_size, hp.img_size, 1),
-                minval=-0.1,
-                maxval=0.1)
+                (hp.img_size, hp.img_size, 1), minval=-0.1, maxval=0.1
+            )
 
         return img
 
     def get_data(self, path, is_vgg, shuffle, augment):
-        """ Returns an image data generator which can be iterated
+        """Returns an image data generator which can be iterated
         through for images and corresponding class labels.
 
         Arguments:
@@ -184,13 +189,15 @@ class Datasets():
             # ============================================================
 
             data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
-                preprocessing_function=self.preprocess_fn)
+                preprocessing_function=self.preprocess_fn
+            )
 
             # ============================================================
         else:
             # Don't modify this
             data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
-                preprocessing_function=self.preprocess_fn)
+                preprocessing_function=self.preprocess_fn
+            )
 
         # VGG must take images of size 224x224
         img_size = 224 if is_vgg else hp.img_size
@@ -205,10 +212,11 @@ class Datasets():
         data_gen = data_gen.flow_from_directory(
             path,
             target_size=(img_size, img_size),
-            class_mode='sparse',
+            class_mode="sparse",
             batch_size=hp.batch_size,
             shuffle=shuffle,
-            classes=classes_for_flow)
+            classes=classes_for_flow,
+        )
 
         # Setup the dictionaries if not already done
         if not bool(self.idx_to_class):
